c4ba007f47e16c6a5868ac52e74051d161f63f27
Justin Fortier <justin.fortier@weboniselab.com>
2019-05-28 11:30:48 -0300

    With SQLAlchemy, switching it out for Peewee because SQLAlchemy is overkill for something as small as this app.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sync2failban',
+    version='0.1dev',
+    url='webonise.com',
+    author='Justin Fortier',
+    author_email='justin.fortier@weboniselab.com',
+    packages=['lib',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
